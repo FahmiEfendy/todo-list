b2dd85cb501cc813fb63fe50ad5e0e06119304d1
Fahmi <itsfahmiefendy@gmail.com>
2023-06-21 17:00:37 +0700

    feat: add todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+
 import "./App.css";
+import TodoList from "./TodoList/TodoList";
+
+const todosArray = [
+  {
+    id: "1",
+    title: "Title 1",
+    completed: false,
+  },
+  {
+    id: "2",
+    title: "Title 2",
+    completed: false,
+  },
+  {
+    id: "3",
+    title: "Title 3",
+    completed: false,
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(todosArray);
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">Todo List App</h1>
+      <TodoList todos={todos} setTodos={setTodos} />
     </>
   );
 }
---
+++ src/TodoList/TodoList.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+interface ITodos {
+  id: string;
+  title: string;
+  completed: boolean;
+}
+
+interface TodoListProps {
+  todos: Array<ITodos>;
+  setTodos: (todos: Array<ITodos>) => void;
+}
+
+const TodoList = (props: TodoListProps) => {
+  const { todos, setTodos } = props;
+
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  const titleChangeHandler = (e: string) => {
+    setEnteredTitle(e);
+  };
+
+  const addTodoHandler = () => {
+    const newTodo = {
+      id: uuidv4(),
+      title: enteredTitle,
+      completed: false,
+    };
+
+    setTodos([...todos, newTodo]);
+    setEnteredTitle("");
+  };
+
+  return (
+    <React.Fragment>
+      <div>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          value={enteredTitle}
+          onChange={(e) => titleChangeHandler(e.target.value)}
+        />
+        <button onClick={addTodoHandler}>Add Todo</button>
+      </div>
+      {todos.map((todo) => {
+        return (
+          <div key={todo.id}>
+            <p>{todo.title}</p>
+          </div>
+        );
+      })}
+    </React.Fragment>
+  );
+};
+
+export default TodoList;
